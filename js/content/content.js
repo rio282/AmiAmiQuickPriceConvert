b16312d9021494cfff47afdc7dfefb92d3b680fb
Rio Bérénos <r.berenos@hotmail.com>
2023-11-12 01:43:27 +0100

    created historyTab

--- js/content/content.js
+++ js/content/content.js
@@ -59,17 +59,75 @@ class ContentManager {
         const id = "historyTab";
 
         if (show) {
-            // add history to top of page
+            // create history tab
             const historyTab = document.createElement("div");
             historyTab.id = id;
-            historyTab.classList.add("historyTab");
+            historyTab.classList.add("row");
+            historyTab.innerHTML = `
+            <h1 class="title">Product Viewing History</h1>
+            <div id="roller">
+            </div>`;
 
+            // add historyTab to top of page
             const topOfPage = document.querySelector(".body");
             topOfPage.prepend(historyTab);
+
+            const roller = historyTab.querySelector("#roller");
+
+            // create cards with items in historyTab
+            const items = Local.getItemsFromLocalStorage();
+            for (const item of items) {
+                const strippedTitle = `${item.thumb_title.substring(0, 50)}...`;
+                const itemLink = `https://www.amiami.com/eng/detail/?scode=${item.key}`;
+                const itemTemplate = `
+                    <div class="card">
+                        <div>
+                            <p class="card-title">${strippedTitle}</p>
+                            <button type="button" role="button" onclick="window.open('${itemLink}', '_blank')">Open</button>
+                        </div>
+                        <img src="https://img.amiami.com/${item.thumb_url}" alt="${item.thumb_alt}" width="150">
+                        <hr class="vr">
+                    </div>`;
+
+                roller.innerHTML += itemTemplate;
+            }
         } else {
             const historyTab = document.getElementById(id);
             historyTab.remove();
         }
+    }
+}
+// --------------------------------------------------------------------------------------------------------------------
+class Local {
+    static #getKeysFromLocalStorage() {
+        const allowedKeyTypes = ["FIGURE", "GOODS"];
+
+        let keys = [];
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const keyType = key.split("-")[0];
+
+            // check if the type of the key is valid (a.k.a. an item on amiami)
+            if (allowedKeyTypes.includes(keyType)) {
+                keys.push(key);
+            }
+        }
+
+        return keys;
+    }
+
+    static getItemsFromLocalStorage() {
+        let items = [];
+
+        const keys = this.#getKeysFromLocalStorage();
+        for (const key of keys) {
+            let item = JSON.parse(localStorage.getItem(key));
+            item.key = key;
+
+            items.push(item);
+        }
+
+        return items;
     }
 }
 
